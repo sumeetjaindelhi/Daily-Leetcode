7d945151779c3cc323bc11002a2064e12baed537
Sumeet Jain <68370214+sumeet2-lab@users.noreply.github.com>
2022-12-20 12:11:17 +0530

    Time: 1 ms (88.80%), Space: 42.3 MB (39.02%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if(s1.equals(s2)) return true;
+        if(s1.length() != s2.length()) return false;
+        ArrayList<Integer> list = new ArrayList<>();
+        int a = 0;
+        boolean toReturn = false;
+        for(int i = 0; i < s1.length(); i++){
+            if(s1.charAt(i) != s2.charAt(i)) 
+                list.add(i);
+        }
+        if(list.size() > 2) 
+            return false;
+        if(list.size() == 2) 
+        {
+            int i = list.get(0) , j = list.get(1);
+            toReturn = (s1.charAt(i) == s2.charAt(j) && s1.charAt(j) == s2.charAt(i));
+            return toReturn;
+        }
+        
+        return toReturn;
+    }
+}
